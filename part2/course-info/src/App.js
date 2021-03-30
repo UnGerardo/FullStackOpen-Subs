a310c4431a65d7146c5d1fb9e09a5f93b78d0bc8
UnGerardo <gerardojdls@gmail.com>
2021-03-30 14:52:08 -0700

    Finished exercise 2.5

--- part2/course-info/src/App.js
+++ part2/course-info/src/App.js
@@ -1,22 +1,5 @@
 import React from 'react'
-
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
-
-const Header = ({ name }) => <h1>{ name }</h1>
-
-const Content = ({ parts }) => parts.map(part => <Part key={part.id} part={part} />)
-
-const Total = ({ parts }) => <p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
-
-const Course = ({ course }) => {
-    return (
-        <>
-            <Header name={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </>
-    );
-}
+import Course from './Course'
 
 const App = () => {
     const courses = [
