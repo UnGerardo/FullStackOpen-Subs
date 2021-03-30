09caaed64952fc8e8a3e2feb9a39fe7168114a47
UnGerardo <gerardojdls@gmail.com>
2021-03-30 12:05:33 -0700

    Finished exercise 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
 
+function Button({ text, handleClick }) {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+function Statistic({ text, data }) {
+  return <p>{text} {data}</p>
+}
+
 function Statistics({ goodCount, neutralCount, badCount }) {
 
   if(goodCount || neutralCount || badCount) {
     return (
       <>
         <h1>statistics</h1>
-        <p>good {goodCount}</p>
-        <p>neutral {neutralCount}</p>
-        <p>bad {badCount}</p>
-        <p>all {goodCount + neutralCount + badCount}</p>
-        <p>average {(goodCount - badCount)/(goodCount + neutralCount + badCount)}</p>
-        <p>positive {goodCount/(goodCount + neutralCount + badCount) * 100} %</p>
+        <Statistic text='good' data={goodCount} />
+        <Statistic text='neutral' data={neutralCount} />
+        <Statistic text='bad' data={badCount} />
+        <Statistic text='all' data={goodCount + neutralCount + badCount} />
+        <Statistic text='average' data={(goodCount - badCount)/(goodCount + neutralCount + badCount)} />
+        <Statistic text='positive' data={`${goodCount/(goodCount + neutralCount + badCount) * 100} %`} />
       </>
     );
   } else {
@@ -33,9 +43,9 @@ function App() {
   return (
     <div className="App">
       <h1>give feedback</h1>
-      <button onClick={() => setGoodCount(goodCount + 1)}>good</button>
-      <button onClick={() => setNeutralCount(neutralCount + 1)}>neutral</button>
-      <button onClick={() => setBadCount(badCount + 1)}>bad</button>
+      <Button text='good' handleClick={() => setGoodCount(goodCount + 1)}/>
+      <Button text='neutral' handleClick={() => setNeutralCount(neutralCount + 1)}/>
+      <Button text='bad' handleClick={() => setBadCount(badCount + 1)}/>
       <Statistics goodCount={goodCount} neutralCount={neutralCount} badCount={badCount} />
     </div>
   );
