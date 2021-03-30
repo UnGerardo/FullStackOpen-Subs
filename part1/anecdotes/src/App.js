e5e93567382b0f4314bd7a3fbf994488627be1d9
UnGerardo <gerardojdls@gmail.com>
2021-03-30 12:58:44 -0700

    Finished exercise 1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -13,12 +13,38 @@ const App = () => {
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState({ 0:0, 1:0, 2:0, 3:0, 4:0, 5:0 });
 
+  const getPopularAnecdote = () => {
+    let maxVotes = {
+      votes: 0,
+      index: 0
+    };
+
+    const keys = Object.keys(votes);
+    
+    for(let i = 0; i < keys.length; i++) {
+      if(votes[i] > maxVotes.votes) {
+        maxVotes.votes = votes[i];
+        maxVotes.index = i;
+      }
+    }
+
+    return (
+      <>
+        <p>{anecdotes[maxVotes.index]}</p>
+        <p>has {maxVotes.votes} votes</p>
+      </>
+    )
+  }
+
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <button onClick={() => setVotes({ ...votes, [selected]: votes[selected] + 1 })}>vote</button>
       <button onClick={() => setSelected(Math.floor(Math.random()*(6)))}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      {getPopularAnecdote()}
     </div>
   )
 }
