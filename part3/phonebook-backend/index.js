7e8cf08280852370cf0dd426ff2953c15d66427d
UnGerardo <gerardojdls@gmail.com>
2021-04-10 16:18:33 -0700

    Finished exercise 3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -29,6 +29,12 @@ app.get('/api/persons', (request, response) => {
     response.json(persons);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+    console.log(request.params);
+    const id = request.params.id;
+    response.json(persons.find(person => person.id = id));
+});
+
 app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people.</p> <p>${new Date()}</p>`)
 })
