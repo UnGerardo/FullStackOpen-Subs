f00878af7c36b8b6414bb42a756bdf0a80e74917
UnGerardo <gerardojdls@gmail.com>
2021-04-26 16:45:17 -0700

    Moved schema and model logic to blog.js

--- part4/blog-list-backend/index.js
+++ part4/blog-list-backend/index.js
@@ -7,15 +7,6 @@ const mongoose = require('mongoose');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
 const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(result => logger.info("Connected to MongoDB"))
