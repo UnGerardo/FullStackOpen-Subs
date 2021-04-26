14a3834ea221b91e88d4d32d5c1ddacbc4496d82
UnGerardo <gerardojdls@gmail.com>
2021-04-26 16:50:02 -0700

    Moved route handling to blogs.js

--- part4/blog-list-backend/index.js
+++ part4/blog-list-backend/index.js
@@ -6,34 +6,17 @@ const mongoose = require('mongoose');
 
 const config = require('./utils/config');
 const logger = require('./utils/logger');
+const blogsRouter = require('./controllers/blogs');
 
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(result => logger.info("Connected to MongoDB"))
     .catch(error => logger.error("ERROR connecting to MongoDB: ", error));
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        });
-})
+app.use('/api/blogs', blogsRouter);
 
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body);
-    
-    blog
-    .save()
-    .then(result => {
-        response.status(201).json(result)
-    });
-})
-
-const PORT = config.PORT;
-app.listen(PORT, () => {
-    logger.info(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`);
 })
