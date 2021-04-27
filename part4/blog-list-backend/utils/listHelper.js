4bef22931d47f923297c3b54bc70b293fa124ce5
UnGerardo <gerardojdls@gmail.com>
2021-04-26 21:37:23 -0700

    Finished exercise 4.5, added new func with tests

--- part4/blog-list-backend/utils/listHelper.js
+++ part4/blog-list-backend/utils/listHelper.js
@@ -9,7 +9,26 @@ const totalLikes = (blogs) => {
                                 blogs.reduce((sum, blog) => sum + blog.likes, 0);
 }
 
+const favoriteBlog = (blogs) => {
+    if(blogs.length === 0) return {};
+
+    let mostLiked = blogs[0];
+
+    for(let i = 1; i < blogs.length; i++) {
+        if(blogs[i].likes > mostLiked.likes) {
+            mostLiked = blogs[i];
+        }
+    }
+
+    return {
+        title: mostLiked.title,
+        author: mostLiked.author,
+        likes: mostLiked.likes
+    };
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 };
