094475099c766315e63b88ba4ea5f46053ded433
UnGerardo <gerardojdls@gmail.com>
2021-04-30 11:30:39 -0700

    Updated routes with async/await syntax

--- part4/blog-list-backend/controllers/blogs.js
+++ part4/blog-list-backend/controllers/blogs.js
@@ -1,15 +1,13 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-blogsRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        });
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({});
+    
+    response.json(blogs);
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body);
 
     if(!blog.title && !blog.url) {
@@ -20,11 +18,9 @@ blogsRouter.post('/', (request, response) => {
         blog.likes = 0;
     }
     
-    blog
-    .save()
-    .then(result => {
-        response.status(201).json(result)
-    });
+    const result = await blog.save();
+    
+    response.status(201).json(result);
 })
 
 module.exports = blogsRouter;
